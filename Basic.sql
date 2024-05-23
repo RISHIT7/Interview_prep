0004a8689c968c63f80d1a716f5b91804bfed84d
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-05-23 12:18:43 +0530

    Create Basic.sql

---
+++ Basic.sql
@@ -0,0 +1,8 @@
+-- Use the editor to create new tables, insert data and all other SQL operations.
+
+-- Literal SELECT statements
+-- SQL uses apostrophe, not doubleinv commas
+SELECT 'Rishit';
+-- SQL uses CSV, so comma can beused to create two columns
+SELECT 'Rishit', 'Jakharia';
+SELECT (5*5) - (5*3);
