8d3a7a35aa9bdc9731dffb35c102f4371740ad3f
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-05-23 12:25:46 +0530

    Update Basic.sql

--- Basic.sql
+++ Basic.sql
@@ -6,3 +6,13 @@ SELECT 'Rishit';
 -- SQL uses CSV, so comma can beused to create two columns
 SELECT 'Rishit', 'Jakharia';
 SELECT (5*5) - (5*3);
+
+-- Basic SELECT statements
+-- SELECT [Column 1], [Column 2]
+-- FROM [Database Name].[Scema Name].[Table Name]
+
+-- SELECT first_name, last_name
+SELECT TOP 3
+FROM Customers;
+SELECT TOP 10 PERCENT first_name, last_name
+FROM Customers;
