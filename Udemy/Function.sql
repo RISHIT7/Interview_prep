2b9fdaac0ec5fc9efaebc94c58d1e2cebff421f4
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-05-23 15:39:53 +0530

    Update Function.sql

--- Udemy/Function.sql
+++ Udemy/Function.sql
@@ -1,1 +1,20 @@
+-- MIN
+SELECT MIN(Price)
+FROM Products;
 
+-- MAX
+SELECT MAX(Price)
+FROM Products;
+
+-- COUNT
+SELECT COUNT(*)
+FROM Products 
+WHERE Price = 18;
+
+-- AVG
+SELECT AVG(Price)
+FROM Products;
+
+-- SUM
+SELECT SUM(Price)
+FROM Products;
