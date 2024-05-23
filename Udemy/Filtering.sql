31041e11a8c261cc6b97ee88cfc89623eaf02c97
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-05-23 14:42:29 +0530

    Update Filtering.sql

--- Udemy/Filtering.sql
+++ Udemy/Filtering.sql
@@ -1,3 +1,32 @@
 -- Intro
-SELECT * FROM Customers;
+SELECT * FROM Customers
+WHERE City = 'Berlin';
+
+SELECT * FROM Customers
+WHERE NOT City = 'Berlin';
+
+SELECT * FROM Customers
 WHERE City = 'Berlin'
+AND PostalCode = 32;
+
+SELECT *
+FROM HumanResoures.Employee
+WHERE BirthDate < '1/1/1980';
+
+-- Using IN Keywords, to reduce th repetitions
+SELECT *
+FROM HumanResources.vEmployee
+WHERE FirstName IN ('Chris', 'Steve', 'Michael', 'Thomas');
+
+-- Using BETWEEN Keyword
+SELECT *
+FROM Sales.vStoreWitDemographics
+WHERE AnuualSales BETWEEN 1000000 AND 2000000
+
+-- wildcard opertors
+-- LIKE says, expect a wildcard char, such that all first_name starts with Mi and then maybe any char 
+-- in RegEx this would be "Mi" + * -> "Mi%"
+SELECT *
+FROM HumanResources.vEmployee
+WHERE FirstName LIKE 'Mi%';
+-- WHERE FirstName LIKE 'Mi_'; -> "Mi" + alphanumeric
