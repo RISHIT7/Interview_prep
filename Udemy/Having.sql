79a381d5bd64db97f205bd1cce4b3ca33215d9cc
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-05-27 09:49:00 +0530

    Create Having.sql

---
+++ Udemy/Having.sql
@@ -0,0 +1,6 @@
+SELECT Column1, Column2
+FROM Schema.TableName
+WHERE {Filtering Condition}
+GROUP BY [Column Name]
+HAVING [Aggregate Function] {comparison operator} [filtering criteria]
+ORDER BY {Column Name, Ordinal, Alias}
