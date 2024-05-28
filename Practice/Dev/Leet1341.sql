a062ad511dd88288ffebd303d697dc8e221272ba
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-05-28 15:30:19 +0530

    Create Leet1341.sql

---
+++ Practice/Dev/Leet1341.sql
@@ -0,0 +1,36 @@
+-- -- user
+-- select top 1 name
+-- from MovieRating
+-- join Users
+-- on Users.user_id = MovieRating.user_id
+-- group by name
+-- order by count(rating) desc, name
+
+-- -- avg rating
+-- select top 1 title
+-- from MovieRating
+-- join Movies
+-- on MovieRating.movie_id = Movies.movie_id
+-- where created_at between '2020-02-1' and '2020-02-28'
+-- group by title
+-- order by avg(rating)
+
+select T1.name as results
+from(
+select top 1 name
+from MovieRating
+join Users
+on Users.user_id = MovieRating.user_id
+group by name
+order by count(rating) desc, name
+) as T1 union all
+select T2.title as results
+from(
+select top 1 title
+from MovieRating
+join Movies
+on MovieRating.movie_id = Movies.movie_id
+where created_at between '2020-02-1' and '2020-02-28'
+group by title
+order by avg(rating)
+) as T2
