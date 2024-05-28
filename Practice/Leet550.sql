d1a04f2b552287e2ad2fa81997568a8fc7ef0929
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-05-28 07:06:35 +0530

    Update Leet550.sql

--- Practice/Leet550.sql
+++ Practice/Leet550.sql
@@ -1,5 +1,6 @@
+-- This code shows all the consequtive logins, we only want the first, thus
 select round(cast((
-    select count(A1.player_id)
+    select distinct count(A1.player_id)
     from Activity A1
     join Activity A2
     on A1.player_id = A2.player_id
@@ -8,3 +9,16 @@ select round(cast((
     select count(distinct player_id)
     from Activity
 ), 2) as fraction
+
+-- this is the correct solution
+select round((count( distinct a1.player_id)*1.0) / (
+    select count(distinct player_id)
+    from Activity
+), 2) as fraction
+from (
+    select player_id, min(event_date) as event_date
+    from Activity
+    group by player_id
+) as a1
+inner join Activity a2 on a1.player_id = a2.player_id
+where datediff(day, a1.event_Date, a2.event_date) = 1
