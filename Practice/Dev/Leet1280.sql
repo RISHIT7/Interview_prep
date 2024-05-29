ede1ce4062f852e9068381693d7ef9cfabfa48b2
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-05-29 11:54:23 +0530

    Update Leet1280.sql

--- Practice/Dev/Leet1280.sql
+++ Practice/Dev/Leet1280.sql
@@ -16,3 +16,18 @@ group by
 order by  
     Students.student_id,
     Subjects.subject_name;
+
+-- CROSS JOINS
+SELECT 
+    s.student_id,
+    s.student_name,
+    sj.subject_name,
+    COUNT(e.student_id) as 'attended_exams'
+FROM Students s CROSS JOIN Subjects sj
+
+LEFT JOIN Examinations e
+ON      s.student_id = e.student_id 
+and sj.subject_name =e.subject_name 
+
+GROUP BY s.student_id,s.student_name, sj.subject_name
+ORDER BY s.student_id, sj.subject_name;
