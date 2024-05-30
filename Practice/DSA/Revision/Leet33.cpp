61c0238f3787ffcb63d2dc27be4eff1340f700bc
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-05-30 12:02:14 +0000

    day 3 dsa added

---
+++ Practice/DSA/Revision/Leet33.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid])
+            {
+                if (nums[mid] > target && nums[low] <= target)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            else
+            {
+                if (nums[mid] < target && nums[high] >= target)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }  
+        return -1;      
+    }
+};
