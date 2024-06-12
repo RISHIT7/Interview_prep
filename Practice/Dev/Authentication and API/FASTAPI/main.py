946276d2d0a79f20bc390c2b75c8d5c2af62c157
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-06-12 17:53:48 +0000

    feat: Aded DELETE operation

--- Practice/Dev/Authentication and API/FASTAPI/main.py
+++ Practice/Dev/Authentication and API/FASTAPI/main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
-from pydantic import BaseModel # for validation
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -28,9 +28,7 @@ def create_post(post: Post): # from the body
     serial_id += 1 # makeshift database
     my_posts.append(post_dict) # makeshift database
     return {'data': post_dict}
-# title: str, body: str <- want to validate the request body
 
-# retrieving a single post
 @app.get('/posts/{id}')
 def get_post(id: int, response: Response):
     if (id < 0) or (id >= len(my_posts)):
@@ -38,3 +36,13 @@ def get_post(id: int, response: Response):
                             detail=f'Post with id: {id} not found')
 
     return {'data': my_posts[id]}
+
+@app.delete('/posts/{id}')
+def delete_post(id: int, response: Response):
+    if (id < 0) or (id >= len(my_posts)):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'Post with id: {id} not found')
+
+    my_posts.pop(id)
+    response.status_code = status.HTTP_204_NO_CONTENT
+    return {'data': f'Post deleted with id: {id}'}
