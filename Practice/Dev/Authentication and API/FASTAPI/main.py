5603b63cbef1ebe5d3c1a3ba7158071bfd2215b1
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-06-12 17:35:47 +0000

    new: Add endpoint to retrieve a single post by ID

--- Practice/Dev/Authentication and API/FASTAPI/main.py
+++ Practice/Dev/Authentication and API/FASTAPI/main.py
@@ -22,10 +22,17 @@ def get_posts():
     return {'data': 'Posts'}
 
 @app.post('/posts')
-def create_post(post: Post):
-    post_dict = post.dict()
+def create_post(post: Post): # from the body
+    post_dict = post.model_dump()
     post_dict['id'] = serial_id
     serial_id += 1 # makeshift database
     my_posts.append(post_dict) # makeshift database
     return {'data': post_dict}
 # title: str, body: str <- want to validate the request body
+
+# retrieving a single post
+@app.get('/posts/{id}')
+def get_post(id: int):
+    if (id < 0) or (id >= len(my_posts)):
+        return {'error': 'Invalid ID'}
+    return {'data': my_posts[id]}
