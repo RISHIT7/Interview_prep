4b2ef40e374f23e965da53441de14d1a6f1c9e05
RISHIT7 <cs1221621@iitd.ac.in>
2024-06-17 13:46:02 +0530

    feat: Update login endpoint to use OAuth2PasswordRequestForm
    This commit updates the `login` endpoint in the `auth.py` file to use the `OAuth2PasswordRequestForm` class from the `fastapi.security.oauth2` module. This change allows the endpoint to receive the username and password as form data instead of a JSON payload. The `user_cred` parameter is replaced with `user_cred: OAuth2PasswordRequestForm = Depends()` in the function signature. The username is accessed using `user_cred.username` instead of `user_cred.email`. The access token is now generated using the `Oauth2.create_access_token` method and returned in the response along with the token type.

--- Practice/Dev/Authentication and API/FASTAPI/app/routers/auth.py
+++ Practice/Dev/Authentication and API/FASTAPI/app/routers/auth.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from .. import database, schemas, models, utils
+from . import Oauth2
 
 router = APIRouter(
     tags=['Authentication']
 )
-
 @router.post('/login')
-def login(user_cred: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_cred.email).first()
+def login(user_cred: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(
+        models.User.email == user_cred.username
+    ).first()
     
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
@@ -20,6 +23,10 @@ def login(user_cred: schemas.UserLogin, db: Session = Depends(database.get_db)):
                             detail='Invalid Credentials')
     
     # create a token
+    access_token = Oauth2.create_access_token(data={
+        'user_id': user.id,
+    })
+    
     # return a token
     
-    return {'token': 'example token '}
+    return {'token': access_token, 'token_type': 'bearer'}
