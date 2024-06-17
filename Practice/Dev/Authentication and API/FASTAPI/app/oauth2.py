2a8e81fd5979ef4959f76ff8ddb2ace128477ff8
RISHIT7 <cs1221621@iitd.ac.in>
2024-06-17 17:30:04 +0530

    feat: Update OAuth2 authentication and post router endpoints
    This commit updates the `oauth2.py` file to include a new function `get_curr_user` that retrieves the current user based on the access token. The function queries the database for the user with the corresponding ID and returns it. The `get_posts` function in the `post.py` file is modified to use the `curr_user` parameter instead of `user_id`. Similarly, the `create_posts`, `get_post`, `delete_post`, and `update_post` functions are updated to use `curr_user` instead of `user_id`. This change improves the authentication and authorization process for the post router endpoints.

--- Practice/Dev/Authentication and API/FASTAPI/app/oauth2.py
+++ Practice/Dev/Authentication and API/FASTAPI/app/oauth2.py
@@ -2,8 +2,9 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
-from . import schemas
+from . import schemas, database, models
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
@@ -44,11 +45,16 @@ def verify_access_token(token: str, credentials_exceptions):
     
     return token_data
 
-def get_curr_user(token: str = Depends(oauth2_scheme)):
+def get_curr_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail='Could not validate credentials',
         headers={'WWW-Authenticate': 'Bearer'}
     )
     
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+    user_id = token.id
+    
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    
+    return user
--- Practice/Dev/Authentication and API/FASTAPI/app/routers/post.py
+++ Practice/Dev/Authentication and API/FASTAPI/app/routers/post.py
@@ -11,12 +11,12 @@ router = APIRouter(
 )
 
 @router.get('/', response_model = List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_curr_user)):
+def get_posts(db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_curr_user)):
     posts = db.query(models.Post).all()
     return posts
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_curr_user)): # from the body
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_curr_user)): # from the body
     new_post = models.Post(**post.model_dump())
     db.add(new_post)
     db.commit()
@@ -24,7 +24,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_i
     return new_post
 
 @router.get('/{id}', response_model = schemas.Post)
-def get_post(id: int,  db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_curr_user)):
+def get_post(id: int,  db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_curr_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     
     if not post:
@@ -34,7 +34,7 @@ def get_post(id: int,  db: Session = Depends(get_db), user_id: int = Depends(oau
     return post
 
 @router.delete('/{id}')
-def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_curr_user)):
+def delete_post(id: int, db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_curr_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     
@@ -47,7 +47,7 @@ def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(o
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put('/{id}', response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_curr_user)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_curr_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     
