ae4b2e6d9cc55359827ec7a81744ff3443f89fbe
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2025-02-08 04:03:21 +0530

    Create LongestConsequtiveSequence.py

---
+++ Practice/DSA/Revision/LongestConsequtiveSequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        table = {}
+        maxval = 0
+        for num in nums:
+            x = table.get(num - 1, 0)
+            y = table.get(num + 1, 0)
+            val = x + y + 1
+            table[num - x] = val
+            table[num + y] = val
+            maxval = max(maxval, val)
+        return maxval
+    
